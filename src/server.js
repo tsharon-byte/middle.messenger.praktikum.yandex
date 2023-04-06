188b284064d6dd52a026c2b7f5b394d8d8e1bfee
Tatiana Churkina <sharonova_t@inbox.ru>
2023-04-06 13:47:31 +0300

    fix tests

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static(`./dist/`));
+
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
 app.listen(3000, () => {
     console.log(`App listening on port 3000!`);
